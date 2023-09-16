534bc8e8e2f1817b7de864bd89faf740327530c3
OlegPanadiy <opanadiy@gmail.com>
2023-09-16 20:30:31 +0200

    commit 1

--- src/js/Api/api-recipe_info.js
+++ src/js/Api/api-recipe_info.js
@@ -1,15 +1,25 @@
 // https://tasty-treats-backend.p.goit.global/api/recipes/
-import axios from "axios";
+import axios from 'axios';
+import Notiflix from 'notiflix';
 // ------------Перелік усіх рецептів----------------
-const BASE_URL = `https://tasty-treats-backend.p.goit.global/api`
+const BASE_URL = `https://tasty-treats-backend.p.goit.global/api`;
+const END_POINT = `/recipes`;
 
 export async function getAllRecipes() {
-    const END_POINT = `/recipes`;
-    try {        
-        const response = await axios
-        .get(`${BASE_URL}${END_POINT}`)      
-        return response.data;
-    } catch (error) {
-        throw new Error(Notiflix.Notify.failure(``));
-    }
-};
+  try {
+    const response = await axios.get(`${BASE_URL}${END_POINT}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(Notiflix.Notify.failure(``));
+  }
+}
+
+export async function getRecipesDetail(id) {
+  const GET_BYID = `\{id}`;
+  try {
+    const response = await axios.get(`${BASE_URL}${END_POINT}${GET_BYID}`);
+    return response.data[0];
+  } catch (error) {
+    throw new Error(Notiflix.Notify.failure(`${elements.onError.textContent}`));
+  }
+}
---
+++ src/js/modal-first.js
@@ -0,0 +1,74 @@
+import { getRecipesDetail } from './Api/api-recipe_info';
+import * as basicLightbox from 'basiclightbox';
+
+const detailBtn = document.querySelector('.see-recipe');
+detailBtn.addEventListener('click', handlerOnClick);
+console.log(itemElement);
+
+function handlerOnClick() {
+  const _id = detailBtn.dataset.id;
+  getRecipesDetail(_id).then(respInfo => {
+    const instance = basicLightbox.create(`
+        <div>
+      <button class="btn">
+        <svg class="on-close" width="10" height="10">
+          <use href="./img/icons.svg/#close-icon"></use>
+        </svg>
+      </button>
+      <iframe
+        min-width="295"
+        min-height="295"
+        src="${respInfo.youtube ? respInfo.youtube : respInfo.thumb}"
+      ></iframe>
+      <h2 class="dish-title">${respInfo.title}</h2>
+      <p class="recipe-rating">${respInfo.rating}</p>
+      <ul class="icon-list">
+        <li class="icon-rating">
+          <svg class="icon-star" width="18" height="18">
+            <use href="./img/icons.svg/#star"></use>
+          </svg>
+        </li>
+        <li class="icon-rating">
+          <svg class="icon-star" width="18" height="18">
+            <use href="./img/icons.svg/#star"></use>
+          </svg>
+        </li>
+        <li class="icon-rating">
+          <svg class="icon-star" width="18" height="18">
+            <use href="./img/icons.svg/#star"></use>
+          </svg>
+        </li>
+        <li class="icon-rating">
+          <svg class="icon-star" width="18" height="18">
+            <use href="./img/icons.svg/#star"></use>
+          </svg>
+        </li>
+        <li class="icon-rating">
+          <svg class="icon-star" width="18" height="18">
+            <use href="./img/icons.svg/#star"></use>
+          </svg>
+        </li>
+      </ul>
+      <div><p class="preparing-time">${respInfo.time} min</p></div>
+    </div>
+    <div class="ingredients-container">
+          <h3>Інгредієнти:</h3>
+          <ul>
+            ${respInfo.ingredients
+              .map(
+                ingredient => `
+              <li>
+                <span class="ingredient-name">${ingredient.name}</span>
+                <span class="ingredient-measure">${ingredient.measure}</span>
+              </li>
+            `
+              )
+              .join('')}
+          </ul>
+        </div>
+         <p class="dish-instructions">${respInfo.instructions}</p>
+          <button type="button" class="btn">Add to favorite</button>
+          <button type="button" class="btn">Give a rating</button>`);
+    instance.show();
+  });
+}
