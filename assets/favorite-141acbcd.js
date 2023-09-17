64a4683d6763b45990af8b4f3a1f3b12067d7af0
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2023-09-17 19:50:14 +0000

    Deploying to gh-pages from @ NickTsalyn/project-MasterchefRecipes@7ba0cfc9b8ac9781ab20d320fa89f584de4bb512 🚀

---
+++ assets/favorite-141acbcd.js
@@ -0,0 +1,20 @@
+import"./pagination-93424437.js";const i=document.querySelector(".fav-list-recipes"),s=document.querySelector(".nothing"),a=document.querySelector(".fav-recipes"),c=document.querySelector(".fav-categories");let t="";const l=[];for(const e of l)console.log("t",e.title),t+=`            <li class="fav-recipe-card">
+    <img class="fav-card-img" src="${e.preview}" alt="${e.title}">
+    <button class="fav-button-heart">
+      <svg class="fav-heart" width="22" height="22">
+        <use href="./img/icons.svg#heart"></use>
+      </svg>
+    </button>
+      <h3 class="fav-card-title">${e.title}</h3>
+      <p class="fav-cart-text">${e.description}</p>
+      <div class="fav-cart-footer">
+        <p class="fav-rate">${e.rating}</p>
+       <div class="fav-cart-stars">
+         <svg class="fav-star" width="18" height="18">
+           <use href="/img/icons.svg#star"></use>
+         </svg>
+       </div>
+        <button class="fav-card-button">See recipe</button>
+      </div>
+  </li>
+`;t==0?(s.style.display="block",a.style.display="none",c.style.display="none"):(s.style.display="none",a.style.display="block",c.style.display="block",i.innerHTML=t);
