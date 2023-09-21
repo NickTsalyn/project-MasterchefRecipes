a57d32d4591f884ae29aa1a58d00faad39f25fcd
OlegPanadiy <opanadiy@gmail.com>
2023-09-21 16:09:08 +0100

    commit-rev1

--- src/js/render.js
+++ src/js/render.js
@@ -10,7 +10,7 @@ export function renderCard(arr) {
         isActive = 'active';
       }
       return ` 
-    <div  class="card-item modal-open-js  ${isActive}" data-id="${_id}" data-category="${category}" style="background: linear-gradient(1deg, rgba(5, 5, 5, 0.60) 4.82%, rgba(5, 5, 5, 0.00) 108.72%), url(${preview}), lightgray -36.5px 0px / 129.2% 112.544% no-repeat; background-size: cover">
+    <div  class="card-item   ${isActive}" data-category="${category}" style="background: linear-gradient(1deg, rgba(5, 5, 5, 0.60) 4.82%, rgba(5, 5, 5, 0.00) 108.72%), url(${preview}), lightgray -36.5px 0px / 129.2% 112.544% no-repeat; background-size: cover">
       <button class="heart" type="button">
         <svg class="icon-heart" width="22" height="22">
         <use href="./img/icons.svg#heart"></use></svg>
@@ -26,7 +26,7 @@ export function renderCard(arr) {
               ${createRatingMarkup(rating)}
             </ul>
           </div>
-         <button class="see-recipe  "  type="button" >See recipe</button>
+         <button class="see-recipe modal-open-js " data-id="${_id}"   type="button" >See recipe</button>
         </div>
       </div>  
     </div>`;
