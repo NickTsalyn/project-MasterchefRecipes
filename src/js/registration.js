7b135226aef682389ef4cd8d83b9c59267dd3697
Anna211291 <Anjutka777@gmail.com>
2023-09-22 10:50:36 -0400

    Summary

--- src/js/registration.js
+++ src/js/registration.js
@@ -10,6 +10,11 @@ const emailPattern =
 button.addEventListener('click', (e) => {
   const content = `
     <div class="modal">
+    <div class="registr-wrapper">
+    <svg class="registr-icon" >
+      <use href="./img/icons.svg#hat-icon" ></use>
+    </svg>
+     </div>
     <h4 class="reg-title">Registration</h4>
       <form class="registration-form">
         <label for="text" class="label">Email
@@ -91,6 +96,11 @@ button.addEventListener('click', (e) => {
   function openLoginModal() {
     const logForm = `
       <div class="modal">
+       <div class="registr-wrapper">
+    <svg class="registr-icon" >
+      <use href="./img/icons.svg#hat-icon" ></use>
+    </svg>
+     </div>
       <h4 class="reg-title">Login</h4>
         <form class="login-form">
           <label for="text" class="label">Email
